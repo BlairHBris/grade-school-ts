cabfd89bf66dd655ecc30369998b9ba4f9a3bc2d
Blair Bristow <blairhbristow@gmail.com>
2022-05-12 11:21:53 -0400

    Add failure

--- grade-school.ts
+++ grade-school.ts
@@ -1,1 +1,18 @@
-export class GradeSchool {}
+import { stringify } from "querystring"
+
+export class GradeSchool {
+    students = {}
+
+    constructor() {
+
+    }
+
+    roster() {
+        return this.students
+    }
+
+    add(name, grade) {
+        this.students  = stringify(grade) [name]
+    }
+
+}
